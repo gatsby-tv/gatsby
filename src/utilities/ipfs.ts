4c93da5e1ecc07f16207f42a2fc364b373d41af1
Jacob Greenway <jagreenway12@gmail.com>
2020-08-01 14:57:58 -0500

    add libp2p webrtc discovery node

--- src/utilities/ipfs.ts
+++ src/utilities/ipfs.ts
@@ -6,7 +6,29 @@ import IPFS from "ipfs";
 // TODO: Add webrtc and websocket (dedicated relay servers) transport options
 
 const ipfs: Promise<any> = IPFS.create({
-    repo: "/ipfs/" + Math.random()
+  // TODO: Set production instance to use the same repo and dev to use random per tab
+  repo: "/ipfs/" + Math.random(),
+  config: {
+    Addresses: {
+      Swarm: [
+        // TODO: Set production and dev urls for signal/relay servers
+
+        // Webrtc peer discovery
+        "/ip4/127.0.0.1/tcp/9090/ws/p2p-webrtc-star",
+        // "/dns4/webrtc-star.gatsby.tv/tcp/9090/wss/p2p-webrtc-star",
+
+        // TODO: Backup websocket-star relay server (webrtc unsupported)
+        // "/ip4/127.0.0.1/tcp/9091/ws/p2p-websocket-star"
+        // "/dns4/websocket-star.gatsby.tv/9091/wss/p2p-websocket-star"
+      ]
+    },
+    Bootstrap: [],
+    Discovery: {
+      webRTCStar: {
+        Enabled: true
+      }
+    }
+  }
 });
 
 export default ipfs;

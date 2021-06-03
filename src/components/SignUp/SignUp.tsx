f2e6351cc4cd95aa8596b8b20a7d214a429f67e8
Calvin Jay Ross <calvinjayross@gmail.com>
2021-06-02 20:25:50 -0500

    adjusted signup page

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import {
   Button,
   Form,
@@ -6,39 +6,38 @@ import {
   Icon,
   ModalProps,
   TextDisplay,
-} from "@gatsby-tv/components";
-import { GatsbyPlain } from "@gatsby-tv/icons";
+} from '@gatsby-tv/components';
+import { GatsbyPlain } from '@gatsby-tv/icons';
 
-import styles from "./SignUp.module.scss";
+import styles from './SignUp.module.scss';
 
 export function SignUp(): React.ReactElement {
   const emailProps = {
-    id: "email",
-    name: "email",
-    label: "Email",
-    //labelHidden: true,
-    placeholder: "Email",
-    //font: theme.font[4],
+    id: 'email',
+    name: 'email',
+    label: 'Email',
+    labelHidden: true,
+    placeholder: 'Email',
     spellCheck: false,
     autoComplete: true,
   };
+
   const channelNameProps = {
-    id: "channel",
-    name: "channel",
-    label: "Channel Name",
-    //labelHidden: true,
-    placeholder: "Channel Name",
-    //font: theme.font[4],
+    id: 'channel',
+    name: 'channel',
+    label: 'Channel Name',
+    labelHidden: true,
+    placeholder: 'Channel Name',
     spellCheck: false,
     autoComplete: false,
   };
+
   const displayNameProps = {
-    id: "display",
-    name: "display",
-    label: "Display Name",
-    //labelHidden: true,
-    placeholder: "Display Name",
-    //font: theme.font[4],
+    id: 'display',
+    name: 'display',
+    label: 'Display Name',
+    labelHidden: true,
+    placeholder: 'Display Name',
     spellCheck: false,
     autoComplete: false,
   };
@@ -50,53 +49,47 @@ export function SignUp(): React.ReactElement {
     </div>
   );
 
-  const sendSignUp = function(event: React.FormEvent) {
+  const sendSignUp = function (event: React.FormEvent) {
     const requestOptions = {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ email: 'emailValue' })
-      };
-      try {
-        fetch('http://localhost:3001/v1/auth/signin', requestOptions)
-            .then(response => response.json())
-            .then(data => console.log('fuck it heres the session key ' + JSON.stringify(data)));
-      } catch (err) {
-        console.log(err);
-      }
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email: 'emailValue' }),
+    };
+    try {
+      fetch('http://localhost:3001/v1/auth/signin', requestOptions)
+        .then((response) => response.json())
+        .then((data) =>
+          console.log('fuck it heres the session key ' + JSON.stringify(data))
+        );
+    } catch (err) {
+      console.log(err);
+    }
 
-      // We don't need the page to reload.
-      event.preventDefault();
-   };
+    // We don't need the page to reload.
+    event.preventDefault();
+  };
 
   const FormMarkup = (
-    <Form onSubmit={sendSignUp}>
-      <div className={styles.FormPiece}>
-        <Form.Field type="text" {...emailProps} className={styles.FormTextBox} />
-      </div>
-      <div className={styles.FormPiece}>
-        <Form.Field type="text" {...channelNameProps} className={styles.FormTextBox} />
-      </div>
-      <div className={styles.FormPiece}>
-        <Form.Field type="text" {...displayNameProps} className={styles.FormTextBox} />
-      </div>
-
+    <Form className={styles.Form} onSubmit={sendSignUp}>
+      <Form.Field type="text" {...emailProps} className={styles.Field} />
+      <Form.Field type="text" {...channelNameProps} className={styles.Field} />
+      <Form.Field type="text" {...displayNameProps} className={styles.Field} />
 
       <Button
         type="submit"
+        onClick={() => alert('This has not been set up yet.')}
         className={styles.Submit}
       >
         Submit
       </Button>
     </Form>
   );
-  
+
   return (
     <>
-      <div className={styles.BigusDivus}>
-        <div className={styles.CardDiv}>
-          {TitleMarkup}
-          {FormMarkup}
-        </div>
+      <div className={styles.Card}>
+        {TitleMarkup}
+        {FormMarkup}
       </div>
       <Fireworks infinite zIndex={-1} />
     </>

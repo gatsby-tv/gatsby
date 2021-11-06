bf6dce990ee4a4aacb680acc8dc81d8d897d5ca1
cjayross <calvinjayross@gmail.com>
2021-11-05 22:18:22 -0500

    bugfix: fixed ambiguous type in session context

--- src/services/session/context.ts
+++ src/services/session/context.ts
@@ -1,4 +1,4 @@
-import { createContext, Dispatch, SetStateAction } from 'react';
+import { createContext, Dispatch } from 'react';
 import { User } from '@gatsby-tv/types';
 
 export type SessionState = {
@@ -8,9 +8,14 @@ export type SessionState = {
   valid: boolean;
 };
 
+export type SessionAction =
+  | string
+  | undefined
+  | ((prevState: SessionState) => string | undefined);
+
 export type SessionContextType = {
   session: SessionState;
-  setSession: Dispatch<SetStateAction<string | undefined>>;
+  setSession: Dispatch<SessionAction>;
   mutate: <T>(value?: T) => T;
 };
 
